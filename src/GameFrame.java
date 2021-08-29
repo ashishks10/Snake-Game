8b31073359fd589aca1812d22e6fcc211d0bdf7e
ashishks10 <67684910+ashishks10@users.noreply.github.com>
2021-08-29 20:17:35 +0530

    Update GameFrame.java

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -4,10 +4,10 @@ public class GameFrame extends JFrame{
 	GameFrame(){
 		 this.add(new GamePanel());								
 		 this.setTitle("Snake Game");							//To set the title of the Frame
-		 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//Exits the application on initiating "close" on the Frame
-		 this.setResizable(false);								//To prevent the user from resizing the frame
-		 this.pack();											//To make the Window size to fit is subcomponents preferred size and layouts 
-		 this.setVisible(true);									//To show the Window
+		 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);				//Exits the application on initiating "close" on the Frame
+		 this.setResizable(false);							//To prevent the user from resizing the frame
+		 this.pack();									//To make the Window size to fit is subcomponents preferred size and layouts 
+		 this.setVisible(true);								//To show the Window
 		 this.setLocationRelativeTo(null);						//To set the location of the Window on the center of the screen
 		 
 	}
